ac202106a77ee962470cb63a847840829e482644
Iria Varela Palmás <iria.varela.palmas@rai.usc.es>
2025-03-11 14:47:01 +0100

    Solucionada nomenclatura y los imports

--- backend_Iria/app/routes/activities.py
+++ backend_Iria/app/routes/activities.py
@@ -2,33 +2,33 @@
 from flask import Blueprint, request, jsonify
 from flask_jwt_extended import jwt_required, get_jwt_identity
 """DEPENDEN DE LA BD"""
-from ..models import db, Task
+from app.models import db, Activity
 
-tasks_bp = Blueprint('tasks', __name__)
+activities_bp = Blueprint('activities', __name__)
 
-@tasks_bp.route('/tasks', methods=['GET'])
+@activities_bp.route('/activities', methods=['GET'])
 @jwt_required()
-def get_tasks():
+def get_activities():
     user_id = get_jwt_identity()
-    tasks = Task.query.filter_by(user_id=user_id).all()
+    activities = Activity.query.filter_by(user_id=user_id).all()
     return jsonify([{
         "id": t.id,
         "title": t.title,
         "due_date": str(t.due_date),
         "completed": t.completed
-    } for t in tasks]), 200
+    } for t in activities]), 200
 
-@tasks_bp.route('/tasks', methods=['POST'])
+@activities_bp.route('/activities', methods=['POST'])
 @jwt_required()
-def add_task():
+def add_activity():
     data = request.json
     user_id = get_jwt_identity()
-    new_task = Task(
+    new_activity = Activity(
         user_id=user_id,
         title=data['title'],
         due_date=data['due_date'],
         completed=False
     )
-    db.session.add(new_task)
+    db.session.add(new_activity)
     db.session.commit()
-    return jsonify({"message": "Tarea añadida"}), 201
+    return jsonify({"message": "Actividad añadida"}), 201
