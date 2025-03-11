f7aa2aa465817952aabda23acda09583d2ae473a
Iria Varela Palmás <iria.varela.palmas@rai.usc.es>
2025-03-11 19:40:43 +0100

    añadidas comprobaciones al get

--- backend_Iria/app/routes/group.py
+++ backend_Iria/app/routes/group.py
@@ -7,68 +7,77 @@ from models import db, GroupAdmin, Participation, Schedule, Activity, Subject, U
 group_bp = Blueprint('group', __name__)
 
 
-@group_bp.route('/<int:group_id>/schedule', methods=['POST'])
+@group_bp.route('/schedule/<int:group_id>', methods=['POST'])
 @jwt_required()
 def add_group_schedule(group_id):
     user_id = get_jwt_identity()
-    
+
     if not GroupAdmin.query.filter_by(user_id=user_id, group_id=group_id).first():
         return jsonify({"message": "Admin access required"}), 403
-    
-    data = request.json
-    
+
+    data = request.get_json()
+
+    if not data or 'name' not in data:
+        return jsonify({"message": "Invalid JSON format"}), 400
+
     new_schedule = Schedule(
         name=data['name'],
-        group_id=group_id,
+        group_id=group_id
     )
     db.session.add(new_schedule)
     db.session.commit()
     
     return jsonify({"message": "Horario añadido"}), 201
 
-@group_bp.route('/<int:group_id>/schedules', methods=['GET'])
+@group_bp.route('/schedules/<int:group_id>', methods=['GET'])
 @jwt_required()
 def get_group_schedules(group_id):
     user_id = get_jwt_identity()
-    if not Participation.query.filter_by(user_id=user_id, group_id=group_id).first():
+    participation = Participation.query.filter_by(user_id=user_id, group_id=group_id).first()
+    if not participation:
         return jsonify({"message": "Group access required"}), 403
-    
+
+    group = db.session.get(Group, group_id)
+    if not group:
+        return jsonify({"message": "Group not found"}), 404
+
     schedule = Schedule.query.filter_by(group_id=group_id).first()
     if not schedule:
         return jsonify({"message": "No schedule found for this group"}), 404
-
-    q = Activity.query.join(TimeSlot.query.filter_by(TimeSlot.schedule_id == schedule.id))
+    
+    q = Activity.query.join(TimeSlot.query.filter_by(schedule_id = schedule.schedule_id))
     schedule_subjects = q.join(Subject).all()
     schedule_user_activities = q.join(UserActivity).all()
+
     schedule_data = {
-        "id": schedule.id,
+        "id": schedule.schedule_id,
         "name": schedule.name,
         "subjects": [
             {
-                "id": sa.id,
+                "id": sa.activity_id,
                 "name": sa.name,
-                "difficulty": sa.difficulty,
-                "priority": sa.difficulty,
-
-                "day_of_week": sa.timeslot.day_of_week,
-                "start_time": sa.timeslot.start_time.isoformat(),
-                "end_time": sa.timeslot.end_time.isoformat(),
-
-                "curriculum": sa.curriculum,
-                "professor": sa.professor
-
+                "difficulty" : sa.difficulty,
+                "priority" : sa.difficulty,
+                
+                "day_of_week": sa.day_of_week,
+                "start_time": sa.start_time.isoformat(),
+                "end_time": sa.end_time.isoformat(),
+                
+                "curriculum" : sa.curriculum,
+                "professor" : sa.professor
+                            
             } for sa in schedule_subjects
         ],
         "user_activities": [
             {
-                "id": sa.id,
+                "id": sa.activity_id,
                 "name": sa.name,
-                "difficulty": sa.difficulty,
-                "priority": sa.difficulty,
-
-                "day_of_week": sa.timeslot.day_of_week,
-                "start_time": sa.timeslot.start_time.isoformat(),
-                "end_time": sa.timeslot.end_time.isoformat(),
+                "difficulty" : sa.difficulty,
+                "priority" : sa.difficulty,
+                
+                "day_of_week": sa.day_of_week,
+                "start_time": sa.start_time.isoformat(),
+                "end_time": sa.end_time.isoformat(),
             } for sa in schedule_user_activities
         ]
     }
