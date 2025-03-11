f9489aa981fb18b8d8521c9b54396da246f1a070
Iria Varela Palmás <iria.varela.palmas@rai.usc.es>
2025-03-11 16:43:59 +0100

    Cambio de nombre de variable name -> username

--- backend_Iria/app/models.py
+++ backend_Iria/app/models.py
@@ -8,7 +8,7 @@ bcrypt = Bcrypt()
 class User(db.Model):
     __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
+    username = db.Column(db.String(100), unique=True, nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
 
