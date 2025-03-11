59a4e2ad6f3944f0e1bd5c9719d2e7a2c27759e6
Iria Varela Palmás <iria.varela.palmas@rai.usc.es>
2025-03-11 16:27:42 +0100

    Añadida API de la informacion de los grupos

---
+++ backend_Iria/app/routes/group.py
@@ -0,0 +1,60 @@
+#archivo rutas de gestion de horarios
+from flask import Blueprint, request, jsonify
+from flask_jwt_extended import jwt_required, get_jwt_identity
+"""DEPENDE DE LA BD"""
+from app.models import db, GroupAdmin, Participation, Schedule, Activity, Subject, TimeSlot, Event, GroupEvent
+
+group_bp = Blueprint('group', __name__)
+
+
+@group_bp.route('/<int:group_id>/schedule', methods=['POST'])
+@jwt_required()
+def add_group_schedule(group_id):
+    user_id = get_jwt_identity()
+    
+    if not GroupAdmin.query.filter_by(user_id=user_id, group_id=group_id).first():
+        return jsonify({"message": "Admin access required"}), 403
+    
+    data = request.json
+    
+    new_schedule = Schedule(
+        name=data['name'],
+        group_id=group_id,
+    )
+    db.session.add(new_schedule)
+    db.session.commit()
+    
+    return jsonify({"message": "Horario añadido"}), 201
+
+@group_bp.route('/<int:group_id>/schedules', methods=['GET'])
+@jwt_required()
+def get_group_schedules(group_id):
+    user_id = get_jwt_identity()
+    if not Participation.query.filter_by(user_id=user_id, group_id=group_id).first():
+        return jsonify({"message": "Group access required"}), 403
+    
+    schedule = Schedule.query.filter_by(group_id=group_id).first()
+    if not schedule:
+        return jsonify({"message": "No schedule found for this group"}), 404
+    
+    schedule_activities = Activity.query.join(TimeSlot.filter(TimeSlot.schedule_id == schedule.id)).join(Subject).all()
+    
+    schedule_data = {
+        "id": schedule.id,
+        "name": schedule.name,
+        "subjects": [
+            {
+                "day_of_week": sa.timeslot.day_of_week,
+                "start_time": sa.timeslot.start_time.isoformat(),
+                "end_time": sa.timeslot.end_time.isoformat(),
+                "id": sa.id,
+                "name": sa.name,
+                "difficulty" : sa.difficulty,
+                "priority" : sa.difficulty,
+                "curriculum" : sa.curriculum,
+                "professor" : sa.professor
+                            
+            } for sa in schedule_activities
+        ]
+    }
+    return jsonify(schedule_data), 200
