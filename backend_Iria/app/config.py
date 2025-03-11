18924d0ffd45cf21433b84a888f326d98b8a1550
Iria Varela Palmás <iria.varela.palmas@rai.usc.es>
2025-03-11 14:44:28 +0100

    Codigo depurado

--- backend_Iria/app/config.py
+++ backend_Iria/app/config.py
@@ -2,7 +2,6 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///app.db")
+    SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "supersecretkey")
-    CORS_HEADERS = 'Content-Type'
