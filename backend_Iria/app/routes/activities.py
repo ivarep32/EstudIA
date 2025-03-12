450c2ff9957a87f8fcaaec0c02ded309c40e5bf9
Iria Varela Palmás <iria.varela.palmas@rai.usc.es>
2025-03-12 10:23:17 +0100

    añadido codigo de prueba de swagger

--- backend_Iria/app/routes/activities.py
+++ backend_Iria/app/routes/activities.py
@@ -1,12 +1,37 @@
 #archivo rutas de tareas y tests
 from flask import Blueprint, request, jsonify
 from flask_jwt_extended import jwt_required, get_jwt_identity
+from flasgger import swag_from
 """DEPENDEN DE LA BD"""
 from app.models import db, Activity
 
 activities_bp = Blueprint('activities', __name__)
 
 @activities_bp.route('/activities', methods=['GET'])
+@swag_from({
+    'tags': ['activities'],
+    'summary': 'Actividades del usuario',
+    'description': 'almacena las actividades y tareas del usuario en el sistema',
+    'responses':
+        {
+        200: {
+            'description': 'Lista de actividades',
+            'schema': {
+                'type': 'array',
+                'items':{
+                    'type': 'object',
+                    'properties': {
+                        'name': {'type': 'string', 'example': 'Estudiar mates'},
+                        'description': {'type': 'string', 'example': 'Estudiar los temas de geometría'},
+                        'difficulty': {'type': 'integer', 'example': 8},
+                        'priority': {'type': 'integer', 'example': 4}
+                    }
+                }
+            }
+        },
+            401: {'description': 'No autorizado'}
+        }
+})
 @jwt_required()
 def get_activities():
     user_id = get_jwt_identity()
@@ -18,6 +43,33 @@ def get_activities():
         "completed": t.completed
     } for t in activities]), 200
 
+
+@swag_from({
+    'tags': ['activities'],
+    'summary': 'Crear una nueva actividad',
+    'description': 'Permite al usuario añadir una nueva actividad',
+    'parameters': [
+        {
+            'name': 'body',
+            'in': 'body',
+            'required': True,
+            'schema':{
+                'type': 'object',
+                'properties':{
+                        'name': {'type': 'string', 'example': 'Estudiar geografía'},
+                        'description': {'type': 'string', 'example': 'Estudiar los rios de europa'},
+                        'difficulty': {'type': 'integer', 'example': 9999},
+                        'priority': {'type': 'integer', 'example': 2}
+                }
+            }
+        }
+    ],
+    'responses':{
+        201:{'description': 'actividad creada exitosamente'},
+        400:{'description': 'datos inválidos'},
+        401:{'description': 'no autorizado'}
+    }
+})
 @activities_bp.route('/activities', methods=['POST'])
 @jwt_required()
 def add_activity():
