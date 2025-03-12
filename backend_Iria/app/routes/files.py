5a3917c4a7c62aeb2016cfbb0739a5a481f7747a
Gael Denise Soto Rodríguez <gaeldenise.soto@rai.usc.es>
2025-03-12 20:08:51 +0100

    Swagger files parte 1

--- backend_Iria/app/routes/files.py
+++ backend_Iria/app/routes/files.py
@@ -13,8 +13,9 @@ if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)  # Ensure upload folder exists
 
 @files_bp.route('/files/<int:group_id>', methods=['GET'])
+
 @jwt_required
-def get_files(group_id):
+def get_files(group_id):    
     user_id = get_jwt_identity()
     
     # Comprobar que o usuario participe no grupo.
@@ -24,9 +25,24 @@ def get_files(group_id):
     files = File.query.filter_by(group_id=group_id).all()
     file_list = [{"id": file.file_id, "filename": file.filename} for file in files]
 
-    return jsonify(file_list)
+    return jsonify(file_list), 200
+
 
 @files_bp.route('/upload/<int:group_id>', methods=['POST'])
+@swag_from({
+    'tags': ['Files'],
+    'summary': 'Upload a file',
+    'description': 'Uploads a file and stores it in a group-specific folder.',
+    'parameters': [
+        {'name': 'group_id', 'in': 'path', 'type': 'integer', 'required': True, 'description': 'ID of the group'},
+        {'name': 'file', 'in': 'formData', 'type': 'file', 'required': True, 'description': 'File to upload'}
+    ],
+    'responses': {
+        200: {'description': 'File uploaded successfully'},
+        400: {'description': 'No file provided'},
+        401: {'description': 'The user has no access to the group'}
+    }
+})
 @jwt_required
 def upload_file(group_id):
     if 'file' not in request.files:
@@ -36,7 +52,7 @@ def upload_file(group_id):
     
     # Comprobar que o usuario participe no grupo.
     if not Participation.query.filter_by(group_id=group_id, user_id=user_id).first():
-        return jsonify({"message": "Can not upload a file to a group the user is not in"}), 401
+        return jsonify({"error": "Can not upload a file to a group the user is not in"}), 401
     
     file = request.files['file']
     if file:
@@ -48,7 +64,7 @@ def upload_file(group_id):
         db.session.add(new_file)
         db.session.commit()
         
-        return jsonify({"message": "File uploaded successfully!", "file_id": new_file.file_id})
+        return jsonify({"message": "File uploaded successfully!", "file_id": new_file.file_id}), 200
 
 @files_bp.route('/download/<int:file_id>', methods=['GET'])
 @jwt_required
