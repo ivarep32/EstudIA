fa9624fef87faf451dd7f70367c26dc32b4b7924
Gael Denise Soto Rodríguez <gaeldenise.soto@rai.usc.es>
2025-03-13 11:27:58 +0100

    Engadida ruta para eliminar archivos

--- backend_Iria/app/routes/files.py
+++ backend_Iria/app/routes/files.py
@@ -99,6 +99,10 @@ def upload_file(group_id):
     if 'file' not in request.files:
         return jsonify({"error": "No file provided"}), 400
 
+    group_folder = os.path.join(UPLOAD_FOLDER, f"/{group_id}")
+    if not os.path.exists(group_folder):
+        os.makedirs(group_folder)  # Ensure upload folder exists
+    
     user_id = get_jwt_identity()
     
     # Comprobar que o usuario participe no grupo.
@@ -108,7 +112,7 @@ def upload_file(group_id):
     file = request.files['file']
     if file:
         filename = secure_filename(file.filename)
-        file_path = os.path.join(UPLOAD_FOLDER, f"/{group_id}", filename)
+        file_path = os.path.join(group_folder, filename)
         file.save(file_path)
 
         new_file = File(filename=file.filename, file_path=file_path, group_id=group_id)
@@ -117,7 +121,7 @@ def upload_file(group_id):
         
         return jsonify({"message": "File uploaded successfully!", "file_id": new_file.file_id}), 200
 
-@files_bp.route('/download/<int:file_id>', methods=['GET'])
+@files_bp.route('/file/<int:file_id>', methods=['GET'])
 @swag_from({
     'tags': ['Files'],
     'summary': 'Download a file',
@@ -158,13 +162,66 @@ def download_file(file_id):
     user_id = get_jwt_identity()
     
     # Comprobar que o usuario participe no grupo.
-    if not Participation.query.filter_by(user_id=user_id).\
-        join(File.query.filter_by(file_id=file_id), File.group_id, Participation.group_id).first():
-        
+    has_access = db.session.query(Participation).join(File, File.group_id == Participation.group_id)\
+        .filter(File.file_id == file_id, Participation.user_id == user_id).first()
+    
+    if not has_access:
         return jsonify({"message": "Can not upload a file to a group the user is not in"}), 401
     
     file = File.query.get(file_id)
+    
     if file:
         return send_file(file.file_path, as_attachment=True, download_name=file.filename)
     
     return jsonify({"error": "File not found"}), 404
+
+
+
+@files_bp.route('/file/<int:file_id>', methods=['DELETE'])
+@swag_from({
+    'tags': ['Files'],
+    'summary': 'Remove a file',
+    'description': 'Removes an specific file.',
+    'parameters': [
+        {
+            'name': 'Authorization',
+            'in': 'header',
+            'required': True,
+            'description': 'Bearer token for authentication',
+            'schema': {
+                'type': 'string',
+                'example': 'Bearer <your_jwt_token>'
+            }
+        },
+        {'name': 'file_id', 'in': 'path', 'type': 'integer', 'required': True, 'description': 'ID of the file'}
+    ],
+    'responses': {
+        200: {'description': 'File removed successfully'},
+        401: {'description': 'The user has no access to the group'}
+    }
+})
+@jwt_required()
+def remove_file(file_id):
+    user_id = get_jwt_identity()
+    
+    has_access = db.session.query(Participation).join(File, File.group_id == Participation.group_id)\
+        .filter(File.file_id == file_id, Participation.user_id == user_id).first()
+    
+    if not has_access:
+        return jsonify({"message": "Can not upload a file to a group the user is not in"}), 401
+
+    file_entry = File.query.get(file_id)
+    if not file_entry:
+        return jsonify({"error": "File not found"}), 404
+    
+    file_path = file_entry.file_path  # Get the file path
+
+    # Check if file exists in the filesystem
+    if os.path.exists(file_path):
+        os.remove(file_path)
+    
+    # Remove the file entry from the database
+    db.session.delete(file_entry)
+    db.session.commit()
+
+    return jsonify({"message": "File deleted successfully"}), 200
